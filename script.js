95e49c48248b32442d4a1ea7119156da62db0c69
hendrikBischoping <h.bischoping@gmx.de>
2025-03-13 16:24:22 +0100

    cleaned and formatted code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let pokedex = [];
 let pokeData = [];
 let isOpened = false;
 
-async function init(){
+async function init() {
     toggleLoadingSpinner();
     await getData();
     renderPokedex();
@@ -25,16 +25,16 @@ async function getData() {
     }
 }
 
-function combineArrays(pokeNames){
+function combineArrays(pokeNames) {
     for (let i = 0; i < pokeNames.length; i++) {
-        pokedex.push({ name: pokeNames[i], data: pokeData[pokeData.length-30+i] });
-      }
+        pokedex.push({ name: pokeNames[i], data: pokeData[pokeData.length - 30 + i] });
+    }
 }
 
-function renderPokedex(){
+function renderPokedex() {
     let pokedexRef = document.getElementById('pokedex');
     pokedexRef.innerHTML = "";
-    for (let inDex= 0; inDex < pokedex.length; inDex++) {
+    for (let inDex = 0; inDex < pokedex.length; inDex++) {
         let pokeName = pokedex[inDex]['name']['name'];
         pokeName = pokeName.charAt(0).toUpperCase() + pokeName.slice(1).toLowerCase();
         let allTypes = pokedex[inDex]['data']['types'];
@@ -43,33 +43,33 @@ function renderPokedex(){
     }
 }
 
-function toggleLoadingSpinner(){
+function toggleLoadingSpinner() {
     let loadingSpinner = document.getElementById('loading_spinner');
-    loadingSpinner.classList.toggle ('d_none');
-    loadingSpinner.classList.toggle ('d_flex');
+    loadingSpinner.classList.toggle('d_none');
+    loadingSpinner.classList.toggle('d_flex');
 }
 
-function findPokemon(){
+function findPokemon() {
     let finderInputRef = document.getElementById('finder_input');
-    let pokeCardRef = document.getElementsByClassName ('pokeCard');
+    let pokeCardRef = document.getElementsByClassName('pokeCard');
     let morePkmnRef = document.getElementById('more_pkmn');
     if (finderInputRef.value.length >= 3) {
         activateFind(finderInputRef, pokeCardRef, morePkmnRef)
-    }else if(finderInputRef.value.length <= 2) {
+    } else if (finderInputRef.value.length <= 2) {
         for (let i = 0; i < pokeCardRef.length; i++) {
-            pokeCardRef[i].classList.remove ('d_none');        
+            pokeCardRef[i].classList.remove('d_none');
         }
-        morePkmnRef.classList.add ('d_flex');
-        morePkmnRef.classList.remove ('d_none');
-        }
+        morePkmnRef.classList.add('d_flex');
+        morePkmnRef.classList.remove('d_none');
+    }
 }
 
-function activateFind(finderInputRef, pokeCardRef, morePkmnRef){
+function activateFind(finderInputRef, pokeCardRef, morePkmnRef) {
     for (let i = 0; i < pokeCardRef.length; i++) {
-        pokeCardRef[i].classList.add ('d_none');        
+        pokeCardRef[i].classList.add('d_none');
     }
-    morePkmnRef.classList.add ('d_none');
-    morePkmnRef.classList.remove ('d_flex');
+    morePkmnRef.classList.add('d_none');
+    morePkmnRef.classList.remove('d_flex');
     let findPokedex = [];
     let foundPokemon = [];
     findPokedex.push(finderInputRef.value);
@@ -78,30 +78,29 @@ function activateFind(finderInputRef, pokeCardRef, morePkmnRef){
     openFoundPokemon(finderInputRef, foundPokemon)
 }
 
-function openFoundPokemon(finderInputRef, foundPokemon, ){
+function openFoundPokemon(finderInputRef, foundPokemon,) {
     pokedex.filter((pokemon) => {
         if (pokemon["name"]["name"].toLowerCase().includes(finderInputRef.value)) {
             foundPokemon.push(pokemon)
         }
     });
     showFoundPokemon(foundPokemon)
-
     if (!finderInputRef.value) {
-    pokeCardRef.classList.remove ('d_none')
-    morePkmnRef.classList.remove ('d_none')
+        pokeCardRef.classList.remove('d_none')
+        morePkmnRef.classList.remove('d_none')
     }
 }
 
-function showFoundPokemon(foundPokemon){
+function showFoundPokemon(foundPokemon) {
     for (let k = 0; k < foundPokemon.length; k++) {
         let selfName = foundPokemon[k]['name']['name'];
         selfName = selfName.charAt(0).toUpperCase() + selfName.slice(1).toLowerCase();
         let foundCard = document.getElementById(selfName);
-        foundCard.classList.remove ('d_none');        
+        foundCard.classList.remove('d_none');
     }
 }
 
-function renderPokeInfo(inDex){
+function renderPokeInfo(inDex) {
     isOpened = true;
     let pokeInfoRef = document.getElementById('poke_info')
     let pokeName = pokedex[inDex]['name']['name'];
@@ -114,57 +113,56 @@ function renderPokeInfo(inDex){
     let pokeSpecialAttack = pokedex[inDex]['data']['stats'][3]['base_stat'];
     let pokeSpecialDefense = pokedex[inDex]['data']['stats'][4]['base_stat'];
     let pokeSpeed = pokedex[inDex]['data']['stats'][5]['base_stat'];
-    pokeInfoRef.innerHTML = ""; 
-        pokeInfoRef.innerHTML += singleCardTemplate(inDex, allTypes, pokeName, pokeType, pokeHp, pokeAttack, pokeDefense, pokeSpecialAttack, pokeSpecialDefense, pokeSpeed)
-        renderStats(pokeHp, pokeAttack, pokeDefense, pokeSpecialAttack, pokeSpecialDefense, pokeSpeed)
-        addPkmnInfo(inDex)
-        toggleScrolling();
+    pokeInfoRef.innerHTML = "";
+    pokeInfoRef.innerHTML += singleCardTemplate(inDex, allTypes, pokeName, pokeType, pokeHp, pokeAttack, pokeDefense, pokeSpecialAttack, pokeSpecialDefense, pokeSpeed)
+    renderStats(pokeHp, pokeAttack, pokeDefense, pokeSpecialAttack, pokeSpecialDefense, pokeSpeed)
+    addPkmnInfo(inDex)
+    toggleScrolling();
 }
 
-function toggleScrolling(){
+function toggleScrolling() {
     let content = document.getElementById('body');
     if (isOpened) {
-        content.classList.add ('prevent_scrolling');
-    } else {body.classList.remove ('prevent_scrolling');}
+        content.classList.add('prevent_scrolling');
+    } else { body.classList.remove('prevent_scrolling'); }
 }
 
-
-
-function addPkmnInfo(){
+function addPkmnInfo() {
     let pokeInfoRef = document.getElementById('poke_info');
-    pokeInfoRef.classList.remove ('d_none');
-    pokeInfoRef.classList.add ('d_flex');
+    pokeInfoRef.classList.remove('d_none');
+    pokeInfoRef.classList.add('d_flex');
 }
 
-function removePkmnInfo(){
+function removePkmnInfo() {
     isOpened = false;
     let pokeInfoRef = document.getElementById('poke_info');
-    pokeInfoRef.classList.add ('d_none');
-    pokeInfoRef.classList.remove ('d_flex');
+    pokeInfoRef.classList.add('d_none');
+    pokeInfoRef.classList.remove('d_flex');
     toggleScrolling()
 }
 
-function toggleSinglePokePic(inDex){
+function toggleSinglePokePic(inDex) {
     let singlePokePic = document.getElementById(`pokePic${inDex}`);
     let singleShinyPokePic = document.getElementById(`shinyPokePic${inDex}`);
-    let singleHeadlineNormal = document.getElementById (`headline_normal${inDex}`);
-    let singleHeadlineShiny = document.getElementById (`headline_shiny${inDex}`); 
-    singlePokePic.classList.toggle ('d_none');
-    singleShinyPokePic.classList.toggle ('d_none');
-    singleHeadlineNormal.classList.toggle ('d_none')
-    singleHeadlineShiny.classList.toggle ('d_none')    
+    let singleHeadlineNormal = document.getElementById(`headline_normal${inDex}`);
+    let singleHeadlineShiny = document.getElementById(`headline_shiny${inDex}`);
+    singlePokePic.classList.toggle('d_none');
+    singleShinyPokePic.classList.toggle('d_none');
+    singleHeadlineNormal.classList.toggle('d_none')
+    singleHeadlineShiny.classList.toggle('d_none')
 }
 
-function goBack(inDex){
+function goBack(inDex) {
     let flippedIndex = inDex - 1;
-    if(flippedIndex < 0){
-        flippedIndex = pokedex.length-1;
+    if (flippedIndex < 0) {
+        flippedIndex = pokedex.length - 1;
     }
     renderPokeInfo(flippedIndex);
 }
-function goNext(inDex){
+
+function goNext(inDex) {
     let flippedIndex = inDex + 1;
-    if(flippedIndex >= pokedex.length){
+    if (flippedIndex >= pokedex.length) {
         flippedIndex = 0;
     }
     renderPokeInfo(flippedIndex);
@@ -174,21 +172,21 @@ function stopBubbling(event) {
     event.stopPropagation();
 }
 
-function toggleStats(inDex){
-    let statsTable = document.getElementById (`stats_table_${inDex}`)
-    statsTable.classList.remove ('d_none')
-    let cardAudios = document.getElementById (`card_audios_${inDex}`)
-    cardAudios.classList.add ('d_none')
+function toggleStats(inDex) {
+    let statsTable = document.getElementById(`stats_table_${inDex}`)
+    statsTable.classList.remove('d_none')
+    let cardAudios = document.getElementById(`card_audios_${inDex}`)
+    cardAudios.classList.add('d_none')
 }
 
-function toggleAudios(inDex){
-    let cardAudios = document.getElementById (`card_audios_${inDex}`)
-    cardAudios.classList.remove ('d_none')
-    let statsTable = document.getElementById (`stats_table_${inDex}`)
-    statsTable.classList.add ('d_none')
+function toggleAudios(inDex) {
+    let cardAudios = document.getElementById(`card_audios_${inDex}`)
+    cardAudios.classList.remove('d_none')
+    let statsTable = document.getElementById(`stats_table_${inDex}`)
+    statsTable.classList.add('d_none')
 }
 
-function loadTypes(inDex){
+function loadTypes(inDex) {
     let typesArray = pokedex[inDex]['data']['types'];
     if (typesArray.length > 1) {
         let typeTwo = pokedex[inDex]['data']['types'][1]['type']['name'];
@@ -196,27 +194,19 @@ function loadTypes(inDex){
     };
 }
 
-function renderStats(pokeHp, pokeAttack, pokeDefense, pokeSpecialAttack, pokeSpecialDefense, pokeSpeed){
+function renderStats(pokeHp, pokeAttack, pokeDefense, pokeSpecialAttack, pokeSpecialDefense, pokeSpeed) {
     var data = [pokeHp, pokeAttack, pokeDefense, pokeSpecialAttack, pokeSpecialDefense, pokeSpeed];
     const labels = ["HP", "ATTACK", "DEFENSE", "SPECIAL ATTACK", "SPECIAL DEFENSE", "SPEED"];
-        const maxData = Math.max(...data);
-    
-        const svg = document.getElementById('chart');
-        const barHeight = 16;
-        const barSpacing = 4;
-
-        data.forEach((value, index) => {
-            const barWidth = (value / maxData) * 250;
-            const x = 20;
-            const y = index * (barHeight + barSpacing) + 20;
-        svg.innerHTML += `
-        <text x="36" y="${y + barHeight / 2 + 5}" class="label">${labels[index]}</text>  
-    `;
-    svg.innerHTML += `
-        <rect x="${x+10}" y="${y}" width="${barWidth}" height="${barHeight}" class="bar"></rect>
-    `;
-    svg.innerHTML += `
-        <text x="${x-20}" y="${y + barHeight / 2 + 5}" class="value">${data[index]}</text>
-    `;
-});
+    const maxData = Math.max(...data);
+    const svg = document.getElementById('chart');
+    const barHeight = 16;
+    const barSpacing = 4;
+    data.forEach((value, index) => {
+        const barWidth = (value / maxData) * 250;
+        const x = 20;
+        const y = index * (barHeight + barSpacing) + 20;
+        svg.innerHTML += `<text x="36" y="${y + barHeight / 2 + 5}" class="label">${labels[index]}</text>`;
+        svg.innerHTML += `<rect x="${x + 10}" y="${y}" width="${barWidth}" height="${barHeight}" class="bar"></rect>`;
+        svg.innerHTML += `<text x="${x - 20}" y="${y + barHeight / 2 + 5}" class="value">${data[index]}</text>`;
+    });
 }
--- template.js
+++ template.js
@@ -1,12 +1,12 @@
-function pokecardTemplate(allTypes, pokeType, inDex, pokeName){
-    return`
+function pokecardTemplate(allTypes, pokeType, inDex, pokeName) {
+    return `
             <div class="pokeCard ${pokeType}_bg" name="${pokeName}" id="${pokeName}" onclick="renderPokeInfo(${inDex})">
                 <div class="card_header d_flex">
-                    <p class="single_index">#${inDex+1}</p>
+                    <p class="single_index">#${inDex + 1}</p>
                     <h2>${pokeName}</h2>
                     <p></p>
                 </div>
-                <img class="pokePic" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${inDex+1}.png" alt="picture of ${pokeName}">
+                <img class="pokePic" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${inDex + 1}.png" alt="picture of ${pokeName}">
                 <div class="type_section">
                     ${getTypesTemplate(allTypes)}
                 </div>
@@ -14,20 +14,20 @@ function pokecardTemplate(allTypes, pokeType, inDex, pokeName){
         `
 }
 
-function singleCardTemplate(inDex, allTypes, pokeName, pokeType){
-    return`
+function singleCardTemplate(inDex, allTypes, pokeName, pokeType) {
+    return `
             <div class="single_card ${pokeType}_bg" id="single_card_${inDex}" onclick="
     stopBubbling(event)">
                 <div class="single_card_header d_flex">
-                    <p class="single_card_index">#${inDex+1}</p>
+                    <p class="single_card_index">#${inDex + 1}</p>
                     <h2 id="headline_normal${inDex}">${pokeName}</h2>
                     <h2 Class="headline_shiny d_none" id="headline_shiny${inDex}">Shiny ${pokeName}</h2>
                     <p class="close_btn" onclick="removePkmnInfo()">×</p>
                 </div>
                 <div class="nav_aria">
                     <img class="arrow_btn" onclick="goBack(${inDex})" src="./content/img/arrow-left.png" alt="arrow_left">
-                    <img class="single_pokePic" id="pokePic${inDex}"onclick="toggleSinglePokePic(${inDex})" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${inDex+1}.png" alt="picture of ${pokeName}">
-                    <img class="single_pokePic d_none" id="shinyPokePic${inDex}"onclick="toggleSinglePokePic(${inDex})" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/${inDex+1}.png" alt="picture of shiny ${pokeName}">
+                    <img class="single_pokePic" id="pokePic${inDex}"onclick="toggleSinglePokePic(${inDex})" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${inDex + 1}.png" alt="picture of ${pokeName}">
+                    <img class="single_pokePic d_none" id="shinyPokePic${inDex}"onclick="toggleSinglePokePic(${inDex})" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/${inDex + 1}.png" alt="picture of shiny ${pokeName}">
                     <img class="arrow_btn" onclick="goNext(${inDex})" src="./content/img/arrow-right.png" alt="arrow_right">    
                     <p class="popup">Click me!</p>
                     <p class="popup d_none">Tap me!</p>
@@ -47,13 +47,13 @@ function singleCardTemplate(inDex, allTypes, pokeName, pokeType){
                                 <div class="single_sound">
                                     <h4>old sound</h4>
                                     <audio controls>
-                                        <source src="https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${inDex+1}.ogg" type="audio/ogg">
+                                        <source src="https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${inDex + 1}.ogg" type="audio/ogg">
                                     </audio>
                                 </div>
                                 <div class="single_sound">
                                     <h4>new sound</h4>
                                     <audio controls>
-                                        <source src="https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/${inDex+1}.ogg" type="audio/ogg">
+                                        <source src="https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/${inDex + 1}.ogg" type="audio/ogg">
                                     </audio>
                                 </div>
                         </div>
